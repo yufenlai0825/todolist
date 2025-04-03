cc171d32c1d742cf4d4b361e641cca0d7880982b
yufenlai0825 <yufenlai0825@gmail.com>
2025-04-03 16:18:18 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+export default {
+  build: {
+    outDir: '../dist',  // Output build to the root dist folder
+  },
+};
