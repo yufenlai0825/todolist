09d662e71b6591f6af646fd64ca3aad852d24c3b
yufenlai0825 <yufenlai0825@gmail.com>
2025-04-04 21:58:42 +0200

    update server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
         '@': path.resolve(__dirname, 'src'), // allows '@/components/Header' inside src folder
       },
     },
-    // server: {
-    //   proxy: {
-    //     '/api': {
-    //       target: process.VITE_BACKEND_URL || 'http://localhost:3000', // backend port
-    //       changeOrigin: true,
-    //       secure: false,
-    //     },
-    //   },
-    // },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.VITE_BACKEND_URL || 'http://localhost:3000', // backend port
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
   });
