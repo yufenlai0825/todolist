23fe94460b708ec43ff1db63f08aa43bfeda8283
yufenlai0825 <yufenlai0825@gmail.com>
2025-04-09 19:46:21 +0200

    update serializeUser

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import pg from "pg"; 
 import pgSession from "connect-pg-simple"; 
 import bcrypt from "bcrypt"; 
-import passport, { serializeUser } from "passport"; 
+import passport from "passport"; 
 import { Strategy } from "passport-local"; 
 import GoogleStrategy from "passport-google-oauth2"; 
 import env from "dotenv"; 
